de9a9d5b482380b0abff393d9044fc391ee7385c
Aditya Pratama Badra <orochimadit@gmail.com>
2021-12-21 05:32:20 +0700

    setup admin LTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DivisionComponent } from './division/division.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DivisionComponent,
+    DashboardComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
